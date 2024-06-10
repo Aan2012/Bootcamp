267b52d3efe2d22518ef376949004fa027fd2475
Aan2012 <120712499+Aan2012@users.noreply.github.com>
2024-06-10 23:33:34 +0700

    Tugas ke 6 Express
    Express JS

--- express.js
+++ express.js
@@ -6,8 +6,10 @@ const port = 3000
 //membaca lokasi css berada 
 app.use('/css', express.static('css'));
 
+//buat array menu
 const arr  = { '/': './xindex.html', '/about': './xabout.html', '/portofolio': './xportofolio.html', '/blog': './xblog.html', '/kontak': './xkontak.html' };
 
+//tampilkan halaman sesuai request dari menu yang dipilih
 Object.entries(arr).forEach(([menu, halaman]) => {
   app.get(menu, (req, res) => {
     res.sendFile(halaman, { root: __dirname });
