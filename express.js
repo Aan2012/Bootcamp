1158b401976a6a23520ab53c900dd50589fe6aa4
Aan2012 <120712499+Aan2012@users.noreply.github.com>
2024-06-10 23:32:13 +0700

    Tugas ke 6
    tugas ke 6 update

--- express.js
+++ express.js
@@ -1,35 +1,26 @@
 const express = require('express')
 const app = express()
+//port webserver yg digunakan
 const port = 3000
 
+//membaca lokasi css berada 
 app.use('/css', express.static('css'));
 
-app.get('/', (req, res) => {
-    res.sendFile('./xindex.html', {root: __dirname});
+const arr  = { '/': './xindex.html', '/about': './xabout.html', '/portofolio': './xportofolio.html', '/blog': './xblog.html', '/kontak': './xkontak.html' };
+
+Object.entries(arr).forEach(([menu, halaman]) => {
+  app.get(menu, (req, res) => {
+    res.sendFile(halaman, { root: __dirname });
+  });
 });
 
-app.get('/about', (req, res) => {
-    //res.send('ini page about');
-    res.sendFile('./xabout.html', {root: __dirname});
-});
-
-app.get('/portofolio', (req, res) => {
-    res.sendFile('./xportofolio.html', {root: __dirname});
-});
-
-app.get('/blog', (req, res) => {
-    res.sendFile('./xblog.html', {root: __dirname});
-});
-
-app.get('/kontak', (req, res) => {
-    res.sendFile('./xkontak.html', {root: __dirname});
-});
-
+//menampilkan status 404 jika halaman tdk ditemukan
 app.use('/',(req, res) => {
     res.status(404);
     res.send('halaman tidak ditemukan!!');
 })
 
+//informasi port yg digunakan
 app.listen(port, () => {
 console.log(`Example app listening on port ${port}`)
 });
